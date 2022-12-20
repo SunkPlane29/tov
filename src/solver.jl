de7b10a121e448e821cd76d3a62f00852028eccb
André <andrezito300@gmail.com>
2022-12-20 20:00:53 -0300

    add some comments, still missing in some files

--- src/solver.jl
+++ src/solver.jl
@@ -8,6 +8,13 @@ using Printf
 #1(unit of length)  = 1.477*10^3m (also one half of the swartzchild radius of the sun)
 #1(unit of time)    = 4.927*10^-6s
 
+#solve_tov is a function that computes the solution of the Tollman-Oppenheimer-Volkoff (TOV) equations
+#given a central pressure of the star and it's equation of state (EoS). stepsize and n (number of points)
+#should also be passed as parameters since each star will have varying radiuses and masses
+#the function returns a struct named curve that holds 3 vectors, one containing the array of radiuses, one
+#containing the array of pressures and one containing the array of masses. The radius array should be in units
+#of m or km (for now it's km), the pressure array can be in any units (but one could use units that help
+#with comparison with other sources) and the mass array should always be in units of solar mass
 function solve_tov(p₀::Real, eos::Function, stepsize::Real, n::Integer)::Curve
     #TODO: i had to choose these initial values very carefully, also, they might affect the solution a bit
     #too much
@@ -17,9 +24,12 @@ function solve_tov(p₀::Real, eos::Function, stepsize::Real, n::Integer)::Curve
     m_init = 1e-24
     p_init = p₀
 
+    #TODO: print statements should not be on modules and non main files
     @printf("Solving TOV with p₀ = %.8e\n", p₀)
 
+    #equation of hydrostatic equilibrium with exception thrown when the pressure is negative
     pressure_eq(r, p, M) = begin
+        #TODO: think of better error management
         if p <= 0
             throw(ErrorException("error: pressure is lesser or equal to zero"))
         end
@@ -31,6 +41,7 @@ function solve_tov(p₀::Real, eos::Function, stepsize::Real, n::Integer)::Curve
         slope = -newtonian#*special_rel_factor1*special_rel_factor2*general_rel_factor
         r == 0 ? 0 : slope
     end
+    #equation of mass continuity
     mass_eq(r, p, M) = begin
         if p <= 0
             throw(ErrorException("error: pressure is lesser or equal to zero"))
@@ -39,6 +50,7 @@ function solve_tov(p₀::Real, eos::Function, stepsize::Real, n::Integer)::Curve
         slope = 4π*r^2*eos(p)
         r == 0 ? 0 : slope
     end
+    #condition function required by the differential system solver
     condition_func(i, r, p, M) = p <= 0 || i > 100000 ? false : true
 
     curve = Curve(Float64[], Float64[], Float64[])
--- src/solveutil.jl
+++ src/solveutil.jl
@@ -1,3 +1,6 @@
+#more generic function that calls solve_tov and catches errors that might appear, will also write the curve
+#data to a .csv file (always named tov_data.csv NOTE: might change later) unless specified not to
+#user must give an initial pressure and an equation of state (function of pressure) for the star
 function solve(p₀::Real, eos::Function; write::Bool = true, stepsize::Real = 200*SI_TO_LENGTH_UNIT, n::Integer = 100000)::Curve
     curve = try solve_tov(p₀, eos, stepsize, n)
         catch err
@@ -12,17 +15,15 @@ function solve(p₀::Real, eos::Function; write::Bool = true, stepsize::Real = 2
     return curve
 end
 
-#TODO: i don't know if there is something wrong with these units or the equation of state, but i get different
-#results from the paper (apparently only on the relativistic neutron star verion, might be a problem with the eos)
-#suggestion use p₀ = 7.463e22 J/m³ for relativistic version white dwarf (and higher power)
-#suggestion use p₀ = 2.488e23 J/m³ for non-relativistic version white dwarf (and higher power)
-#FIXME: non-relativistic white dwarf giving totally different results for the pressure given
+#FIXME: neutron_star giving totally innacurate results
+#solve_plot calls solve and plots curves of pressure and mass in a single plot
 function solve_plot(p₀::Real, eos::Function; stepsize::Real = 200*SI_TO_LENGTH_UNIT, n::Integer = 100000)
     curve = solve(p₀, eos, stepsize = stepsize, n = n)
 
     plot_curves(curve, raw"r(km)", raw"p(erg/cm³)", raw"M (M$_{\odot}$)", "single_star_plot.png")
 end
 
+#solve_data calls solve but does not plot, only writing the data to a file
 function solve_data(p₀::Real, eos::Function; stepsize::Real = 200*SI_TO_LENGTH_UNIT, n::Integer = 100000)
     curve = solve(p₀, eos, stepsize = stepsize, n = n)
 
