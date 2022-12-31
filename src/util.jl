0d79d07d56cd4d32373e52a2139b8a48acf0ccd6
André <andrezito300@gmail.com>
2022-12-31 13:02:28 -0300

    fix error

--- src/util.jl
+++ src/util.jl
@@ -56,8 +56,8 @@ end
 function get_eos_from_csv(file::String)::Function
     eos_data = CSV.File(file, header = ["epsilon", "p"]) |> DataFrame
 
-    pressure = eos.p .* TOV.MEVFM3_TO_MEV4 .* TOV.MEV4_TO_JOULE .* TOV.SI_TO_PRESSURE_UNIT
-    energy_density = eos.epsilon .* TOV.MEVFM4_TO_MEV4 .* TOV.MEV4_TO_JOULE .* TOV.SI_TO_PRESSURE_UNIT
+    pressure = eos_data.p .* TOV.MEVFM3_TO_MEV4 .* TOV.MEV4_TO_JOULE .* TOV.SI_TO_PRESSURE_UNIT
+    energy_density = eos_data.epsilon .* TOV.MEVFM4_TO_MEV4 .* TOV.MEV4_TO_JOULE .* TOV.SI_TO_PRESSURE_UNIT
 
 
     eos_interp = linear_interpolation(pressure, energy_density)
