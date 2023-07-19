e700395324f11918aaa4538e5518506a12caa27e
André <andrezito300@gmail.com>
2023-07-19 20:32:26 -0300

    add method that uses DifferentialEquations.jl

--- src/solver.jl
+++ src/solver.jl
@@ -57,3 +57,53 @@ function solve_tov(p₀::Real, eos::Function, stepsize::Real)::Curve
 
     return curve
 end
+
+# -------------------------------------------------------------------------
+#
+#   NEW CODE FOR SOLVING USING DIFFEQ LIBRARY
+#
+# -------------------------------------------------------------------------
+
+#NOTE: using DifferentialEquations package I should cite the author of this code
+using SciMLBase
+using OrdinaryDiffEq
+
+const ErrorPressureZero = "error: pressure is lesser or equal to zero"
+
+struct TOVSolution
+    p₀::Real,
+    r::AbstractVector{Real},
+    p::AbstractVector{Real},
+    M::AbstractVector{Real},
+end
+
+#NOTE r_max can be changed (for example in solving for white dwarfs)
+function solve_tov(p₀::Real, eos::Function ; r_init::Real=0, r_max::Real=10e5*SI_TO_LENGTH_UNIT)::Curve
+    # don't know why I was still using 1e-24
+    m_init = 0.0
+    p_init = p₀
+
+    pressure_eq(r, p, M) = begin
+        r == 0 ? 0 : (-eos(p)*M/r^2)*(1 + p/eos(p))*(1 + 4πr^3*p/M)*(1 - 2M/r)^(-1)
+    end
+    mass_eq(r, p, M) = begin
+        4π*r^2*eos(p)
+    end
+
+    f(du, u, p, t) = begin
+        du[1] = pressure_eq(t, u[1], u[2])
+        du[2] = mass_eq(t, u[1], u[2])
+    end
+
+    u0 = [p_init, m_init]
+    tspan = (r_init, r_max)
+    prob = ODEProblem(f, u0, tspan)
+
+    # TODO: find a way to call a solve method that allows to break the solving at any point
+
+    condition(u, t, integrator) = u[1] <= 0
+    affect!(integrator) = terminate!(integrator)
+    cb = DiscreteCallback(condition, affect!)
+    # Canonical Runge-Kutta Order 4 method. Uses adaptive stepping.
+    sol = solve(prob, RK4(), callback = cb)
+end
