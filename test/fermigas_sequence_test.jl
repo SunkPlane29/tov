1b936495b328f343fa7bedb8e450ec363118f76e
sunkplane <andrezito300@gmail.com>
2023-08-20 07:25:45 -0300

    before trying to fix numerical artifacts

--- test/fermigas_sequence_test.jl
+++ test/fermigas_sequence_test.jl
@@ -8,11 +8,11 @@ using Plots
 
 # BASEPATH = dirname(dirname(pathof(TOV)))
 BASEPATH = ".."
-# eosfile = joinpath(BASEPATH, "test", "eos", "fermigas.csv")
+eosfile = joinpath(BASEPATH, "test", "eos", "fermigas.csv")
 # eosfile = joinpath(BASEPATH, "test", "eos", "fermigaslarge.csv")
 # eosfile = joinpath(BASEPATH, "test", "eos", "quarkmatter.csv")
-eosfile = joinpath(BASEPATH, "test", "eos", "cmf.csv")
-# eosheader = ["p", "ϵ"]
+# eosfile = joinpath(BASEPATH, "test", "eos", "cmf.csv")
+eosheader = ["p", "ϵ"]
 # eosheader = ["ρb", "p", "ϵ"]
 # eosheader = ["T", "n_b", "Y_q", "p", "ϵ"]
 eos = TOV.eos_from_file(eosfile, eosheader)
@@ -21,14 +21,19 @@ p₀ = eos.pressure
 
 #1e-7 para fermigas
 #6e-5 para quarkmatter
+#1e-7 para cmf
+# sol = TOV.solve_sequence(p₀, eos, eps=6e-5)
 sol = TOV.solve_sequence(p₀, eos, eps=1e-7)
 
-#TODO: as is it solves MR diagrams pretty well, besides the problem
-#of having the plot filled with artifacts
+#TODO: maybe I can get the order of magnitude of the pressure that
+#has converged and use that same order of magnitude in all the other
+#pressures (this would rely on channels I think)
 
-println(sol.R)
-println(sol.M)
-
-# plot(sol.p₀, sol.R, xlim=(-10, 500), show=true)
-# plot(sol.p₀, sol.M, xlim=(-10, 1000), show=false)
-plot(sol.R, sol.M)
+plot(sol.p₀, sol.R, xlim=(-10, 250), ylim=(5, 20), seriestype=:scatter,
+     show=true, size=(1200, 900))
+savefig("p0R.png")
+plot(sol.p₀, sol.M, seriestype=:scatter, xlim=(-10, 1000), show=false,
+     size=(1200, 900))
+savefig("p0M.png")
+plot(sol.R, sol.M, seriestype=:path, size=(1200, 900))
+savefig("MR.png")
